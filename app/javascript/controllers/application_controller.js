b3789eea7798f515152a93bb1a6a6652520adc50
bibi-oliveira <gabrieloliveira663@icloud.com>
2024-04-28 21:13:17 -0300

    navbar update

--- app/javascript/controllers/application_controller.js
+++ app/javascript/controllers/application_controller.js
@@ -1,16 +1,40 @@
 import { Controller } from '@hotwired/stimulus';
 
 export default class extends Controller {
-  static targets = ['menu', 'lista'];
+  static targets = ['lista', 'menu'];
+  timeoutId = null;
 
-  // Dropdown List
-  toggleLista() {
-    this.listaTarget.classList.toggle('hidden');
+  // Método para mostrar a lista
+  showLista() {
+    clearTimeout(this.timeoutId); // Limpa qualquer tempo de espera pendente
+    this.listaTarget.classList.remove('hidden');
   }
 
-  // mobile menu
+  // Método para ocultar a lista com um atraso
+  hideListaWithDelay() {
+    this.timeoutId = setTimeout(() => {
+      this.listaTarget.classList.add('hidden');
+    }, 50); // Tempo de espera em milissegundos (50ms)
+  }
+
+  // Método para limpar o tempo de espera
+  clearListaTimeout() {
+    clearTimeout(this.timeoutId);
+  }
+
+  // Manipuladores de eventos para a lista
+  onListaMouseEnter() {
+    this.clearListaTimeout(); // Limpa qualquer tempo de espera pendente
+    this.showLista(); // Mostra a lista imediatamente
+  }
+
+  onListaMouseLeave() {
+    this.hideListaWithDelay(); // Oculta a lista com um atraso
+  }
+
+   // mobile menu
   toggleMenu() {
     this.menuTarget.classList.toggle('hidden');
-    console.log('foi')
   }
+
 }
